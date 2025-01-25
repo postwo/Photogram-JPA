b90e0934c8aae3c441c891ec48562e92a2f99aed
postwo <codo7717@naver.com>
2025-01-25 14:53:43 +0900

    Transactional을 거는이유 주석 과 reademe 추가

--- src/main/java/com/example/photogram/service/AuthService.java
+++ src/main/java/com/example/photogram/service/AuthService.java
@@ -14,6 +14,8 @@ public class AuthService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptpasswordEncoder;
 
+    // @Transactional을 사용하는 이유: 회원가입 처리의 전 과정을 원자성, 일관성, 자동 롤백으로 안전하게 관리하기 위해서입니다.
+    // 쓰기 작업(INSERT, UPDATE, DELETE)이 포함된 경우 트랜잭션으로 묶어야 데이터가 깨지거나 불완전하게 저장되는 일을 방지
     @Transactional //회원가입 메서드 실행되고 종료 될때까지 진행됨 //Transactional 붙일때는 write(insert,update,delete) 일때 붙여준다
     public User 회원가입(User user) { //여기서 user는 통신을 통해 받아온 user objcet이다
         // 회원가입 진행
