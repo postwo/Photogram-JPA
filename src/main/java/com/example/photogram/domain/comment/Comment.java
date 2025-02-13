cc0933b15d87316d0e91df716558715d0863e937
postwo <codo7717@naver.com>
2025-02-13 18:38:57 +0900

    comment 테이블, repository 생성

---
+++ src/main/java/com/example/photogram/domain/comment/Comment.java
@@ -0,0 +1,45 @@
+package com.example.photogram.domain.comment;
+
+import com.example.photogram.domain.image.Image;
+import com.example.photogram.domain.user.User;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 100, nullable = false)
+    private String content;
+
+    @JoinColumn(name = "imageId")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Image image; //어떤이미지에 작성했는지
+
+    @JsonIgnoreProperties({"images"})
+    @JoinColumn(name = "userId")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User user; //누가 작성했는지
+
+
+    private LocalDateTime createDate;
+
+    @PrePersist //데이터베이스 인서트되기 직전에 실행 //이거는 jpa save등을 사용할때 적용 네이티브쿼리를 사용하면 적용이 안된다.
+    public void createDate() {
+        this.createDate = LocalDateTime.now();
+    }
+
+}
